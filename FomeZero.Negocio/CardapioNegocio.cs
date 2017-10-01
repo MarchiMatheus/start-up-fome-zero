9e722127bca774e1b9802471dc55f60b3dd8c43d
MarchiMatheus <diniz.marchi@gmail.com>
2017-10-01 15:08:22 -0300

    Evolução da solução de FomeZero

--- FomeZero.Negocio/CardapioNegocio.cs
+++ FomeZero.Negocio/CardapioNegocio.cs
@@ -1,12 +1,11 @@
-﻿using FomeZero.Entidades;
-using FomeZero.Entidades.Dominio;
-using FomeZero.Negocio.Entidades.Out;
-using System.Collections.Generic;
+﻿using FomeZero.Negocio.Entidades.Out;
 
 namespace FomeZero.Negocio
 {
     public class CardapioNegocio
     {
+        private LancheNegocio lancheNegocio = new LancheNegocio();
+
         #region [ Obtém cardápio ]        
 
         /// <summary>
@@ -17,113 +16,13 @@ namespace FomeZero.Negocio
         {
             ObterCardapioOut obterCardapioOut = new ObterCardapioOut();
 
-            obterCardapioOut.Cardapio.Add(ObterXBacon());
-            obterCardapioOut.Cardapio.Add(ObterXBurguer());
-            obterCardapioOut.Cardapio.Add(ObterXEgg());
-            obterCardapioOut.Cardapio.Add(ObterXEggBacon());
+            obterCardapioOut.Cardapio.Add(lancheNegocio.ObterXBacon());
+            obterCardapioOut.Cardapio.Add(lancheNegocio.ObterXBurguer());
+            obterCardapioOut.Cardapio.Add(lancheNegocio.ObterXEgg());
+            obterCardapioOut.Cardapio.Add(lancheNegocio.ObterXEggBacon());
 
             return obterCardapioOut;
         }
-
-        #endregion
-
-        #region [ Métodos privados ]
-
-        #region [ Obter X-Bacon ]
-
-        /// <summary>
-        /// Obtém lanche X-Bacon
-        /// </summary>
-        /// <returns></returns>
-        private Lanche ObterXBacon()
-        {
-            List<Ingrediente> ingredientes = new List<Ingrediente>
-            {
-                new Ingrediente { Nome = NomeIngrediente.Bacon },
-                new Ingrediente { Nome = NomeIngrediente.HamburguerCarne },
-                new Ingrediente { Nome = NomeIngrediente.Queijo }
-            };
-
-            return new Lanche
-            {
-                Nome = NomeLanche.XBacon,
-                Ingredientes = ingredientes
-            };
-        }
-
-        #endregion
-
-        #region [ Obter X-Burguer ]
-
-        /// <summary>
-        /// Obtém lanche X-Burguer
-        /// </summary>
-        /// <returns></returns>
-        private Lanche ObterXBurguer()
-        {
-            List<Ingrediente> ingredientes = new List<Ingrediente>
-            {
-                new Ingrediente { Nome = NomeIngrediente.HamburguerCarne },
-                new Ingrediente { Nome = NomeIngrediente.Queijo }
-            };
-
-            return new Lanche
-            {
-                Nome = NomeLanche.XBurguer,
-                Ingredientes = ingredientes
-            };
-        }
-
-        #endregion
-
-        #region [ Obter X-Egg ]
-
-        /// <summary>
-        /// Obtém lanche X-Egg
-        /// </summary>
-        /// <returns></returns>
-        private Lanche ObterXEgg()
-        {
-            List<Ingrediente> ingredientes = new List<Ingrediente>
-            {
-                new Ingrediente { Nome = NomeIngrediente.Ovo },
-                new Ingrediente { Nome = NomeIngrediente.HamburguerCarne },
-                new Ingrediente { Nome = NomeIngrediente.Queijo }
-            };
-
-            return new Lanche
-            {
-                Nome = NomeLanche.XEgg,
-                Ingredientes = ingredientes
-            };
-        }
-
-        #endregion
-
-        #region [ Obter X-Egg Bacon ]
-
-        /// <summary>
-        /// Obtém lanche X-Egg Bacon
-        /// </summary>
-        /// <returns></returns>
-        private Lanche ObterXEggBacon()
-        {
-            List<Ingrediente> ingredientes = new List<Ingrediente>
-            {
-                new Ingrediente { Nome = NomeIngrediente.Ovo },
-                new Ingrediente { Nome = NomeIngrediente.Bacon },
-                new Ingrediente { Nome = NomeIngrediente.HamburguerCarne },
-                new Ingrediente { Nome = NomeIngrediente.Queijo }
-            };
-
-            return new Lanche
-            {
-                Nome = NomeLanche.XEggBacon,
-                Ingredientes = ingredientes
-            };
-        }
-
-        #endregion
 
         #endregion
     }
---
+++ FomeZero.Negocio/LancheNegocio.cs
@@ -0,0 +1,105 @@
+﻿using FomeZero.Entidades;
+using FomeZero.Entidades.Dominio;
+using System.Collections.Generic;
+
+namespace FomeZero.Negocio
+{
+    public class LancheNegocio
+    {
+        #region [ Obter X-Bacon ]
+
+        /// <summary>
+        /// Obtém lanche X-Bacon
+        /// </summary>
+        /// <returns></returns>
+        public Lanche ObterXBacon()
+        {
+            List<Ingrediente> ingredientes = new List<Ingrediente>
+            {
+                new Ingrediente { Nome = NomeIngrediente.Bacon },
+                new Ingrediente { Nome = NomeIngrediente.HamburguerCarne },
+                new Ingrediente { Nome = NomeIngrediente.Queijo }
+            };
+
+            return new Lanche
+            {
+                Nome = NomeLanche.XBacon,
+                Ingredientes = ingredientes
+            };
+        }
+
+        #endregion
+
+        #region [ Obter X-Burguer ]
+
+        /// <summary>
+        /// Obtém lanche X-Burguer
+        /// </summary>
+        /// <returns></returns>
+        public Lanche ObterXBurguer()
+        {
+            List<Ingrediente> ingredientes = new List<Ingrediente>
+            {
+                new Ingrediente { Nome = NomeIngrediente.HamburguerCarne },
+                new Ingrediente { Nome = NomeIngrediente.Queijo }
+            };
+
+            return new Lanche
+            {
+                Nome = NomeLanche.XBurguer,
+                Ingredientes = ingredientes
+            };
+        }
+
+        #endregion
+
+        #region [ Obter X-Egg ]
+
+        /// <summary>
+        /// Obtém lanche X-Egg
+        /// </summary>
+        /// <returns></returns>
+        public Lanche ObterXEgg()
+        {
+            List<Ingrediente> ingredientes = new List<Ingrediente>
+            {
+                new Ingrediente { Nome = NomeIngrediente.Ovo },
+                new Ingrediente { Nome = NomeIngrediente.HamburguerCarne },
+                new Ingrediente { Nome = NomeIngrediente.Queijo }
+            };
+
+            return new Lanche
+            {
+                Nome = NomeLanche.XEgg,
+                Ingredientes = ingredientes
+            };
+        }
+
+        #endregion
+
+        #region [ Obter X-Egg Bacon ]
+
+        /// <summary>
+        /// Obtém lanche X-Egg Bacon
+        /// </summary>
+        /// <returns></returns>
+        public Lanche ObterXEggBacon()
+        {
+            List<Ingrediente> ingredientes = new List<Ingrediente>
+            {
+                new Ingrediente { Nome = NomeIngrediente.Ovo },
+                new Ingrediente { Nome = NomeIngrediente.Bacon },
+                new Ingrediente { Nome = NomeIngrediente.HamburguerCarne },
+                new Ingrediente { Nome = NomeIngrediente.Queijo }
+            };
+
+            return new Lanche
+            {
+                Nome = NomeLanche.XEggBacon,
+                Ingredientes = ingredientes
+            };
+        }
+
+        #endregion
+    }
+}
